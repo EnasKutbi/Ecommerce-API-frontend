10552fa3a3a431682a6eff0276e57f6a63e75be6
EnasKutbi <enaskutbi@gmail.com>
2024-05-29 05:45:07 +0300

    update home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,6 +6,17 @@ export const Home = () => {
   return (
     <div>
       <PageTitle title={"Home"} />
+      <section id="home" className="home">
+        <h1>Welcome To Harry Potter World</h1>
+        <p>
+          Here is everything related to the Wizarding World of Harry Potter.
+          <br />
+          Our store specializes in selling Harry Potter products. <br />
+          There is no fun for the Muggles here, only for witches and wizards.
+          <br />
+          All Potterheads are welcome.
+        </p>
+      </section>
       <Products />
     </div>
   )
